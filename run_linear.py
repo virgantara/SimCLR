eae678da17b22d899833856b7a6e126eaa969c3f
virgantara <vinux.edu@gmail.com>
2025-06-05 11:00:36 +0700

    update

--- run_linear.py
+++ run_linear.py
@@ -5,12 +5,15 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import torchvision.models as models
 from models.linear_model import LinearClassifier
+from tqdm import tqdm
 
 def load_pretrained_encoder(filepath):
-    checkpoint = torch.load(filepath, map_location='cpu')
+    checkpoint = torch.load(filepath, map_location='cpu',weights_only=False)
     state_dict = checkpoint['state_dict']
+
     backbone = models.resnet18()
     num_ftrs = backbone.fc.in_features
+    
     backbone.fc = nn.Identity()
     new_state_dict = {}
     for k, v in state_dict.items():
@@ -29,10 +32,14 @@ transform = transforms.Compose([
 train_dataset = datasets.CIFAR10(root='./data', train=True, transform=transform, download=True)
 train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
 
+test_dataset = datasets.CIFAR10(root='./data', train=False, transform=transform, download=True)
+test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 filepath = 'pretrain/checkpoint_0200.pth.tar'
 backbone, feat_dim = load_pretrained_encoder(filepath)
-model = LinearClassifier(backbone, feature_dim=feat_dim)
-model = model.cuda()
+model = LinearClassifier(backbone, feature_dim=feat_dim).to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.classifier.parameters(), lr=1e-3)
@@ -40,7 +47,9 @@ optimizer = optim.Adam(model.classifier.parameters(), lr=1e-3)
 for epoch in range(10):
     model.train()
     total_loss = 0
-    for imgs, labels in train_loader:
+    correct = 0
+    total = 0
+    for imgs, labels in tqdm(train_loader):
         imgs, labels = imgs.cuda(), labels.cuda()
         outputs = model(imgs)
         loss = criterion(outputs, labels)
@@ -48,4 +57,27 @@ for epoch in range(10):
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
-    print(f"Epoch {epoch+1}, Loss: {total_loss / len(train_loader):.4f}")
+
+        _, pred = outputs.max(1)
+        correct += pred.eq(labels).sum().item()
+        total += labels.size(0)
+    
+    acc = 100. * correct / total
+    print(f"Epoch {epoch+1}, Loss: {total_loss / len(train_loader):.4f}, Accuracy: {acc:.2f}%")
+
+
+torch.save(model.state_dict(), 'linear_classifier.pth')
+
+model.eval()
+correct = 0
+total = 0
+with torch.no_grad():
+    for imgs, labels in tqdm(test_loader):
+        imgs, labels = imgs.to(device), labels.to(device)
+        outputs = model(imgs)
+        _, pred = outputs.max(1)
+        correct += pred.eq(labels).sum().item()
+        total += labels.size(0)
+
+acc = 100. * correct / total
+print(f"Test Accuracy: {acc:.2f}%")
