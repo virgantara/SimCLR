5d955f9bd435260508dda6bafa506f0079502d65
virgantara <vinux.edu@gmail.com>
2025-07-10 06:44:34 +0700

    + BYOL

---
+++ download_medmnist.py
@@ -0,0 +1,45 @@
+from tqdm import tqdm
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision.transforms as transforms
+
+import medmnist
+from medmnist import INFO, Evaluator
+
+print(f"MedMNIST v{medmnist.__version__} @ {medmnist.HOMEPAGE}")
+
+data_flag = 'dermamnist'
+# data_flag = 'breastmnist'
+download = True
+
+NUM_EPOCHS = 3
+BATCH_SIZE = 128
+lr = 0.001
+
+info = INFO[data_flag]
+task = info['task']
+n_channels = info['n_channels']
+n_classes = len(info['label'])
+
+DataClass = getattr(medmnist, info['python_class'])
+
+
+# preprocessing
+data_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[.5], std=[.5])
+])
+
+# load the data
+train_dataset = DataClass(split='train', transform=data_transform, download=download)
+test_dataset = DataClass(split='test', transform=data_transform, download=download)
+
+pil_dataset = DataClass(split='train', download=download)
+
+# encapsulate data into dataloader form
+train_loader = data.DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+train_loader_at_eval = data.DataLoader(dataset=train_dataset, batch_size=2*BATCH_SIZE, shuffle=False)
+test_loader = data.DataLoader(dataset=test_dataset, batch_size=2*BATCH_SIZE, shuffle=False)
